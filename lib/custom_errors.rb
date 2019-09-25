c835b564551c55d89fb8ece2e8c97741724c2db3
Dominique Skaf <doskaf@yahoo.com>
2019-09-25 17:03:41 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,17 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
+    end
+  end
+  
+  class PartnerError < StandardError
+    def message
+      
+    end
   end
 
 end
@@ -15,10 +25,6 @@ end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-class PartnerError < StandardError
-  
-end
 
 
 
